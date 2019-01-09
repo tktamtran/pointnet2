470f534f474aeb30309b149f1028f1ae3ca364e0
Thi Kim Tam Tran <tranthi@atcremers50.informatik.tu-muenchen.de>
2019-01-10 00:51:05 +0100

    tf operator scripts functional

--- tf_ops/grouping/tf_grouping_compile.sh
+++ tf_ops/grouping/tf_grouping_compile.sh
@@ -13,4 +13,4 @@ CUDA_ROOT=/usr/stud/tranthi/cuda:/usr/local/cuda-10.0
 CUDA_LIB=/usr/stud/tranthi/cuda/lib64:/usr/local/cuda-10.0/lib64
 CUDA_INC=/usr/stud/tranthi/cuda/include:/usr/local/cuda-10.0/include
 
-g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I $TF_INC -I /usr/local/cuda-10.0/include -I $TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-10.0/lib64/ -L$TF_ROOT -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I $TF_INC -I /usr/local/cuda-10.0/include -I $TF_INC/external/nsync/public -lcudart -L/usr/local/cuda-10.0/lib64/ -L$TF_ROOT -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
